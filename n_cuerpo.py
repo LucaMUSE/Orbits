f1313c071f90128d287c8a3994683afa07cbc17f
LucaMUSE <lucaveneruso01@gmail.com>
2024-11-21 12:17:55 +0100

    n_cuerpos

--- n_cuerpo.py
+++ n_cuerpo.py
@@ -1,10 +1,10 @@
 from numpy import array , matrix, reshape, zeros
 
-def F (U,t):
+def n_cuerpos (U,t):
     
     #Nc = dimension en el espacio
     #Nb = numero de body
-    #2 = posicion = 0; velocidad = 1
+    #2 posicion = 0; velocidad = 1
     
     pv = reshape (U,(Nb,Nc,2)) 
     r = reshape (U[:,:,0], (Nb,Nc)) 
@@ -16,3 +16,4 @@ def F (U,t):
     dvdt = reshape(pFs[:,:,1],(Nb,Nc))
     drdt = v 
     
+
